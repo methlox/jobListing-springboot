fdb2611bddf2d3bf697e1c30eee3827642ae194c
methlox <astroyellowh@gmail.com>
2023-09-21 11:53:08 +0530

    controller repo and models

---
+++ server/src/main/java/com/sid/server/controller/PostController.java
@@ -0,0 +1,9 @@
+package com.sid.server.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class PostController {
+}
