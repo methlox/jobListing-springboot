e4d658511983ef3ea023daac9ad5f1d3cb0ce9ca
methlox <astroyellowh@gmail.com>
2023-09-22 21:58:55 +0530

    dashboard page ui update

--- jobs-springUI/src/pages/Dashboard.jsx
+++ jobs-springUI/src/pages/Dashboard.jsx
@@ -3,8 +3,6 @@ import { Box, Tab, Typography } from "@mui/material";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
 import Create from "./Create";
 
 export default function Home() {
@@ -18,12 +16,9 @@ export default function Home() {
       <Box
         sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
       >
-        <Typography variant="h3" sx={{ margin: "2%" }} align="center">
+        <Typography variant="h3" sx={{ margin: "2%", color: "white", fontFamily: "Eczar" }} align="center">
           EMPLOYER DASHBOARD
         </Typography>
-        <Button sx={{ margin: "2% 3%" }} variant="outlined">
-          <Link to="/">Home</Link>
-        </Button>
       </Box>
       <Box sx={{ width: "100%", typography: "body1" }}>
         <TabContext value={value}>
